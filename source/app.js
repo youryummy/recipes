f8103a962695aa21c7a6deca4bb2980280781619
José Luis Alonso Rocha <joseluisalro20@gmail.com>
2022-12-03 17:05:01 +0100

    Initial commit
    Co-Authored-By: almafe2510 <44414361+Almafe2510@users.noreply.github.com>

---
+++ source/app.js
@@ -0,0 +1,51 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes');
+var recipesRouter = require('./routes/recipes');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/recipes', recipesRouter);
+
+//setup connection to mongo
+const mongoose = require("mongoose");
+const DB_URL = (process.env.DB_URL || "mongodb://localhost/test");
+
+mongoose.connect(DB_URL);
+const db = mongoose.connection;
+
+//recover from errors
+db.on("error", console.error.bind(console, "db connection error"));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
