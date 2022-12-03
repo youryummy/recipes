e4e28036c25ce1ef3ad97cac3680eea952c60621
José Luis Alonso Rocha <joseluisalro20@gmail.com>
2022-12-03 20:40:31 +0100

    feat: basic tasty communication and parse implemented
    WIP
    
    Co-Authored-By: almafe2510 <44414361+Almafe2510@users.noreply.github.com>

--- source/services/tastyService.js
+++ source/services/tastyService.js
@@ -12,7 +12,36 @@ const options = {
 
 const getTastyRecipes = async function() {
     axios.request(options).then(function (response) {
-        console.log(response.data);
+
+        for (var i in response.data.results) {
+            console.log('Receta '+i+':')
+            console.log('Nombre: '+response.data.results[i].name);
+            console.log('Descripcion: '+((response.data.results[i].description == '' || response.data.results[i].description == null) ? ('No hay descripcion') :
+                (response.data.results[i].description.replace(/<[^>]*>/g,"").replace("\n",""))));
+            console.log('Duracion: '+((response.data.results[i].total_time_minutes == '' || response.data.results[i].total_time_minutes == null) ? (0) : (response.data.results[i].total_time_minutes)) +' min.');
+            console.log('Instrucciones:')
+            if(response.data.results[i].instructions==null||response.data.results[i].instructions.length==0)
+                console.log('No hay instrucciones');
+            else
+                for (var j in response.data.results[i].instructions) {
+
+                    console.log('Paso '+(j)+': ')
+                    console.log(response.data.results[i].instructions[j].display_text);
+                    console.log('')
+                }
+
+            console.log('Etiquetas: ')
+            if(response.data.results[i].topics==null||response.data.results[i].topics.length==0)
+                console.log('No hay etiquetas');
+            else
+                for (var k in response.data.results[i].topics) {
+                    console.log(response.data.results[i].topics[k].name);
+
+                }
+            console.log('-------------------------')
+        }
+
+
     }).catch(function (error) {
         console.error(error);
     })
