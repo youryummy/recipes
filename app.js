6145a8affc8d05302d0ede3eb90fd931a544340b
almafe2510 <44414361+Almafe2510@users.noreply.github.com>
2023-01-10 18:33:51 +0100

    fix: update mongo db url
    to connect it to mongo atlas
    
    Co-Authored-By: José Luis Alonso Rocha <joseluisalro20@gmail.com>

--- app.js
+++ app.js
@@ -73,11 +73,7 @@ const mongoProto = process.env.MONGO_PROTO ?? 'mongodb';
 const mongoUser = process.env.MONGO_USER;
 const mongoPwd = process.env.MONGO_PWD;
 
-const mongoURL = `${mongoProto}://` +
-    `${mongoUser ? mongoUser + ":" : ""}` +
-    `${mongoPwd ? mongoPwd + "@" : ""}` +
-    `${mongoHost}${mongoProto == "mongodb+srv" ? "" : ":" + mongoPort}` +
-    `/${mongoDBName}`;
+const mongoURL = `mongodb+srv://almafe2510:clECKtEmZ07a1MbL@recipes-service.0vy5klz.mongodb.net/?retryWrites=true&w=majority`;
 console.log("Connecting to database: %s", mongoURL);
 mongoose.connect(mongoURL);
 mongoose.set('strictQuery', false);
