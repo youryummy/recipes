eb26392ac6ac1d7b406f163d8f292d5676e496b4
almafe2510 <44414361+Almafe2510@users.noreply.github.com>
2023-01-14 21:20:39 +0100

    feat: add CI improvement
    Co-Authored-By: José Luis Alonso Rocha <joseluisalro20@gmail.com>

--- app.js
+++ app.js
@@ -66,19 +66,8 @@ const mongoose = require("mongoose");
 // Node environment
 const env = process.env.NODE_ENV ? process.env.NODE_ENV : 'production';
 
-// Mongo connection variables
-const mongoPort = process.env.MONGO_PORT ?? 27017;
-const mongoHost = process.env.MONGO_HOST ?? 'localhost';
-const mongoDBName = process.env.MONGO_DBNAME ?? 'default-db';
-const mongoProto = process.env.MONGO_PROTO ?? 'mongodb';
-const mongoUser = process.env.MONGO_USER;
-const mongoPwd = process.env.MONGO_PWD;
 
-const mongoURL = `${mongoProto}://` +
-    `${mongoUser ? mongoUser + ":" : ""}` +
-    `${mongoPwd ? mongoPwd + "@" : ""}` +
-    `${mongoHost}${mongoProto == "mongodb+srv" ? "" : ":" + mongoPort}` +
-    `/${mongoDBName}`;
+const mongoURL = `mongodb+srv://almafe2510:clECKtEmZ07a1MbL@recipes-service.0vy5klz.mongodb.net/?retryWrites=true&w=majority`;
 console.log("Connecting to database: %s", mongoURL);
 mongoose.set('runValidators', true);
 mongoose.connect(mongoURL);
