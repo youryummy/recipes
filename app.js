6d0b32747f6de726fe7e601038c80f9e95f09b34
almafe2510 <44414361+Almafe2510@users.noreply.github.com>
2023-01-16 18:57:46 +0100

    fix: more changes
    Co-Authored-By: José Luis Alonso Rocha <joseluisalro20@gmail.com>

--- app.js
+++ app.js
@@ -72,11 +72,7 @@ const mongoDBName = process.env.MONGO_DBNAME ?? 'default-db';
 const mongoProto = process.env.MONGO_PROTO ?? 'mongodb';
 const mongoUser = process.env.MONGO_USER;
 const mongoPwd = process.env.MONGO_PWD;
-const mongoURL = `${mongoProto}://` +
-`${mongoUser ? mongoUser + ":" : ""}` +
-    `${mongoPwd ? mongoPwd + "@" : ""}` +
-    `${mongoHost}${mongoProto == "mongodb+srv" ? "" : ":" + mongoPort}` +
-    `/${mongoDBName}`;
+const mongoURL = `mongodb+srv://almafe2510:clECKtEmZ07a1MbL@recipes-service.0vy5klz.mongodb.net/?retryWrites=true&w=majority`;
 console.log("Connecting to database: %s", mongoURL);
 mongoose.set('runValidators', true);
 mongoose.connect(mongoURL);
